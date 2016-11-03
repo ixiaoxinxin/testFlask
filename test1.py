9f5b5a2f9f29f16299c2be1e6cf582b7b6742b14
Anniez <295563386@qq.com>
2016-11-03 17:06:52 +0800

    https://github.com/ixiaoxinxin/testFlask

--- test1.py
+++ test1.py
@@ -1,12 +1,22 @@
-from flask import request
-from flask import make_response
 from flask import Flask
 from flask import render_template
 from flask_bootstrap3 import Bootstrap
+from flask_script import Manager
+from flask.ext.moment import Moment
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+manager = Manager(app)
+moment = Moment(app)
+from datetime import datetime
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
 
 @app.route('/')
 def index():
@@ -16,13 +26,13 @@ def index():
     #response = make_response('<h1>this document carries a cookie</h1>')
     #response.set_cookie('awser','42')
     #return response
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
     #return '<h1>hello,%s!</h1>' % name
-    return render_template('user.html', name)
-
+    return render_template('user.html', name=name)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
