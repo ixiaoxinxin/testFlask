587468bb5833d332ec04d7aae41987ea0f5e8a93
Anniez <295563386@qq.com>
2016-11-03 14:13:35 +0800

    https://github.com/ixiaoxinxin/testFlask

--- testUnit1/test1.py
+++ testUnit1/test1.py
@@ -1,22 +1,27 @@
 from flask import request
 from flask import make_response
-import flask
+from flask import Flask
+from flask import render_template
+from flask_bootstrap3 import Bootstrap
 
-app = flask(__name__)
-
+app = Flask(__name__)
+bootstrap = Bootstrap(app)
 
 
 @app.route('/')
 def index():
-    user_agent = request.headers.get('User_agent')
+    #user_agent = request.headers.get('User_agent')
     #return '<p>your browser is %s</p>' % user_agent
     #return '<p>bad request</p>' ,400
-    response = make_response('<h1>this document carries a cookie</h1>')
-    response.set_cookie('awser','42')
-    return response
+    #response = make_response('<h1>this document carries a cookie</h1>')
+    #response.set_cookie('awser','42')
+    #return response
+    return render_template('index.html')
+
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>hello,%s!</h1>' % name
+    #return '<h1>hello,%s!</h1>' % name
+    return render_template('user.html', name)
 
 
 if __name__ == '__main__':
