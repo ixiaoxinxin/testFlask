203af36cb991135c3ff02c99e5e37c5dc98ab746
Anniez <295563386@qq.com>
2016-11-07 14:13:03 +0800

    数据库

--- hello.py
+++ hello.py
@@ -5,21 +5,50 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import Required
 from flask_sqlalchemy import SQLAlchemy
+from flask_script import Shell
+from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail, Message
 import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
+from threading import Thread
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = \
-    'sqlite:///' + os.path.join(basedir,'data.sqlite')
-
-#app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True #noneed
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = True
-app.config['SECRET_KEY'] = 'hard to guess string'
-
 db = SQLAlchemy(app)
 manager = Manager(app)
 bootstrap = Bootstrap(app)
+migrate = Migrate(app, db)
+manager.add_command('db', MigrateCommand)
+mail = Mail(app)
+#========================================================
+basedir = os.path.abspath(os.path.dirname(__file__))
+#=========================================================
+app.config['SQLALCHEMY_DATABASE_URI'] = \
+    'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+#app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True #noneed
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS '] = True
+app.config['SECRET_KEY'] = 'hard to guess string'
+app.config['MAIL_SERVER'] = 'smtp.qq.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('shhhdkjqfbogbihi')
+app.config['FLASKY_MAIL_SUBJECT_PREDIX'] = ['Flasky']
+app.config['FLASY_MAIL_SENDER'] = 'Flasky admin<295563386@qq.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+
+
+def send_async_email(app,msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREDIX'] + subject, sender=app.config['FLASKY_MAIL_SENDER'], recipients =[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+    thr = Thread(target=send_async_email, args=[app.msg])
+    thr.start()
+    return thr
 
 
 class Role(db.Model):
@@ -48,24 +77,33 @@ def index():
     #name = None
     form = NameForm()
     if form.validate_on_submit():
-        user = User.query.fifter_by(username = form.name.data).first()
+        user = User.query.filter_by(username=form.name.data).first()
         #old_name = session.get('name')
         #if old_name is not None and old_name != form.name.data:
             #flash('looks like you have changed your name!')
         #session['name'] = form.name.data
         #form.name.data = ''
         if user is None:
-            user = User(username = form.name.data)
+            user = User(username=form.name.data)
             db.session.add(user)
+            session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_mail(app.config['FLASKY_ADMIN'],'New User','mail/new_user',user=user)
         else:
-            session['known']=True
+            session['known'] = True
+        session['name'] = form.name.data
+        form.name.data=''
         return redirect(url_for('index'))
-    return render_template('index.html', form=form, name=session.get('name'),known = session.get('known',False))
+    return render_template('index.html', form=form, name=session.get('name'), known=session.get('known', False))
 
 
 @app.route('/user/<name>')
 def user(name):
     return render_template('user.html', name=name)
+
+def make_shell_context():
+    return dict(app=app, db=db, User=User,Role=Role)
+manager.add_command("shell", Shell(make_context=make_shell_context))
 
 
 if __name__ == '__main__':
