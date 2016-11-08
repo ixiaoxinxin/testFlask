8fc1530e7669a999cbbdbcb5ad365048515813f2
Anniez <295563386@qq.com>
2016-11-08 14:04:28 +0800

    大型程序的结构

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
 from flask_moment import Moment
@@ -10,14 +10,19 @@ mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 
-def creat_app(config_name):
+
+def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config[config_name])
-    config[config_name].init_app()
+    config[config_name].init_app(app)
 
     bootstrap.init_app(app)
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+
+    from main import main as main_blueprint
+
+    app.register_blueprint(main_blueprint)
 
     return app
