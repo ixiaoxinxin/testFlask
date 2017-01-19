a8eda55d1c407761af6a9a3bf0a6b13271a729a5
Anniez <295563386@qq.com>
2017-01-19 15:46:39 +0800

    1.19

--- pythontest/diguikuaisupaixu.py
+++ pythontest/diguikuaisupaixu.py
@@ -1,12 +1,10 @@
 import random
 
-
 list1 = [2, 3, 8, 4, 9, 5, 6]
 list2 = [5, 6, 10, 17, 11, 2]
-
-
 def qsort(L):
-   if len(L)<2: return L
+   if len(L)<2:
+      return L
    pivot_element = random.choice(L)
    small = [i for i in L if i< pivot_element]
    large = [i for i in L if i> pivot_element]
